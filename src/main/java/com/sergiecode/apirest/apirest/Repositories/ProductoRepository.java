a1de488f19ec42b75dad3426a93e03fe06caaf2f
LeandroSinich <leandrosinich@gmail.com>
2024-05-10 10:42:09 -0300

    initial commit

---
+++ src/main/java/com/sergiecode/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.sergiecode.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.sergiecode.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+
+
+}
